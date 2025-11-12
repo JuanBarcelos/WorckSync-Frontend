74452e83e6f460ca0b85ed40c903c489ad431f60
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-11-12 11:49:52 -0300

    update: create employer form control

--- src/app/components/employer-modal/employer-modal.ts
+++ src/app/components/employer-modal/employer-modal.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PrimaryButton } from "../shared/primary-button/primary-button";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employer-modal',
-  imports: [PrimaryButton],
+  imports: [PrimaryButton, ReactiveFormsModule],
   templateUrl: './employer-modal.html',
   styleUrl: './employer-modal.scss',
 })
@@ -11,4 +12,16 @@ export class EmployerModal {
   @Input() visible = false;
   @Output() close = new EventEmitter<void>();
   @Output() apply = new EventEmitter<any>();
+
+  employerForm = new FormGroup({
+    IDEmployer: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required]),
+    document: new FormControl('', [Validators.required]),
+    position: new FormControl('', [Validators.required]),
+    department: new FormControl('', [Validators.required]),
+    admissionDate: new FormControl('', [Validators.required]),
+    shiftId: new FormControl('', [Validators.required]),
+  })
 }
